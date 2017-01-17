540b175365ae267175bb709a244f8bec6d108cc9
Yan Uehara <yanuehara@hotmail.com>
2017-01-17 17:10:58 -0300

    Classes Edge and Graph added
    class Edge contains the origin, destiny and weight of an edge
    
    Graph contains a vector of lists of Edges
    insertEdge() method in Graph to inset an Edge
    
    Signed-off-by: Yan Uehara <yanuehara@hotmail.com>

--- utils.h
+++ utils.h
@@ -8,10 +8,48 @@
 ***********************************************************/
 
 #include <math.h>
+#include <vector>
+#include <list>
+
+using namespace std;
 
 #ifndef UTILS_H
 #define UTILS_H
 
 float pointDistance(int x_inicial, int y_incial, int x_final, int y_final);
+
+class Edge{
+	public:
+		int origin;
+		int destiny;
+		int weight;
+
+		Edge(int x, int y, int w){
+			this->origin = x;
+			this->destiny = y;
+			this->weight = w;
+		}
+
+		bool operator==(const Edge& n1) const{
+			if(n1.origin == this->origin && n1.destiny == this->destiny && n1.weight == this->weight)
+				return true;
+			return false;
+		}
+};
+
+class Graph{
+	private:
+		vector< list<Edge> > grafo;
+	public:
+		Graph(int tam){
+			this->grafo.clear();
+			this->grafo.resize(tam);
+		}
+
+		void insertEdge(int x, int y, int w){
+			Edge* toInsert = new Edge(x, y, w);
+			grafo[x].push_back(*toInsert);
+		}
+};
 
 #endif
