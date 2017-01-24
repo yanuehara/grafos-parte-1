d00dfc376ef0bb5d291ec75c00bc53a247091fd1
Flávio Augusto Muller <f.a.m.shinzato@gmail.com>
2017-01-24 12:52:05 -0300

    Corrected case in utils.cpp and output file in .gitignore
    Signed-off-by: Flávio Augusto Muller <f.a.m.shinzato@gmail.com>

--- utils.cpp
+++ utils.cpp
@@ -1,4 +1,4 @@
-#include  "Utils.h"
+#include  "utils.h"
 
 float pointDistance(int x_inicial, int y_incial, int x_final, int y_final){
 	return sqrt( pow(x_final-x_inicial,2) + pow(y_final - y_incial, 2) );
