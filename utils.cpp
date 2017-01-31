35097e78d809a4c8fcefd44dbc53bc0d1ee84294
Yan Uehara <yanuehara@hotmail.com>
2017-01-31 19:40:02 -0300

    Added comentaries and moved implementation to utils.cpp file
    Signed-off-by: Yan Uehara <yanuehara@hotmail.com>
    Reviewed-By: Flavio Augusto Muller <f.a.m.shinzato@gmail.com>

--- utils.cpp
+++ utils.cpp
@@ -1,13 +1,97 @@
-/********************************************************
+/**********************************************************
 *
 * Flavio Shinzato
 * Yan Uehara
 *
 * Trabalho 1 de Grafos
 * Prof. Said Sadique
+*
+* utils.cpp
+* Arquivo com a implementação de classes e funções auxiliares
+*
 ***********************************************************/
+
 #include  "utils.h"
 
 float pointDistance(int x_inicial, int y_inicial, int x_final, int y_final){
 	return sqrt( pow(x_final-x_inicial,2) + pow(y_final - y_inicial, 2) );
+}
+
+//Método para adicionar uma aresta
+void Graph::addEdge(int x_inicial, int y_inicial, int x_final, int y_final){
+	//Corrige a coordenada do vértice de origem
+	int org = correctCoordinates(x_inicial, y_inicial);
+
+	//Corrige a coordenada do vértice de destino
+	int dest = correctCoordinates(x_final, y_final);
+
+	//Insere uma aresta com a distância entre os dois vértices
+	grafo[org][dest].distance = grafo[dest][org].distance = pointDistance(x_inicial, y_inicial, x_final, y_final);
+}
+
+//Método para remover um vértice do grafo
+void Graph::removeVertex(int x, int y){
+	//Corrige a coordenada
+	int vertex = correctCoordinates(x, y);
+
+	//Inicializa as arestas como infinito
+	for(int i = 0; i < NUMVERTEX; i++){
+		grafo[vertex][i].distance = INT_MAX;
+		grafo[i][vertex].distance = INT_MAX;
+	}
+}
+
+//Método que utiliza o algoritmo de djikstra para achar
+// a distância entre dois pontos
+float Graph::dijkstra(int x_inicial, int y_incial, int x_final, int y_final){
+	//Vetor que guarda a distância entre o vértice inicial e os vértices do grafo
+	// inializado como infinito
+	vector<float> dist(NUMVERTEX, INT_MAX);
+
+	//Lista de prioridades para escolher a próxima aresta no algoritmo dijkstra
+	priority_queue< pair<int, float>, vector<pair<int, float> >, Comparator > pq;
+
+	//Corrige a coordenada
+	int org = correctCoordinates(x_inicial, y_incial);
+	int dest = correctCoordinates(x_final, y_final);
+
+	//A distância da origem para a origem é 0
+	dist[org] = 0.0f;
+
+	//Adiciona a aresta na fila de prioridades com a ordem igual a sua distância
+	pq.push(make_pair(org, dist[org]));
+
+	//Enquanto a fila não for vazia
+	while(!pq.empty()){
+		//Obtém o primeiro elemento da lista (o que contém a menor distância)s
+		int u = pq.top().first; //É o vértice origem
+		float w = pq.top().second; //É a distancia desse vértice até a origems
+
+		//Se u é o destino a distância foi encontrada e o laço termina
+		if(u == dest) break;
+
+		//Remove e elimina a aresta da fila
+		pq.pop();
+
+		//Relaxação da aresta (u i)
+		for(int i = 0; i < NUMVERTEX; i++){
+			//Testa se a aresta existe, se não o laço passa para o próximo vértice
+			if(grafo[u][i].distance == INT_MAX)
+				continue;
+
+			//Calcula a distância de da origem até i passando pela aresta (u i)
+			float alt = w + grafo[u][i].distance;
+			if(alt < dist[i]){
+				//Se a distância até a aresta i, passando por u é menor
+				// do que a distância atuals, atualiza o vetor distância com a nova distância
+				dist[i] = alt;
+
+				//Insere o vértice i, com ordem igual a sua distância
+				pq.push(make_pair(i, dist[i]));
+			}
+		}
+	}
+
+	//Retorna a distância do vértice de origem até o vértice de destino
+	return dist[dest];
 }
--- utils.h
+++ utils.h
@@ -5,6 +5,10 @@
 *
 * Trabalho 1 de Grafos
 * Prof. Said Sadique
+*
+* utils.h
+* Arquivo com a definição de classes e funções auxiliares
+*
 ***********************************************************/
 
 #include <math.h>
@@ -21,12 +25,22 @@ using namespace std;
 #define NUMVERTEX 10201
 #define TAMMATRIX 101
 
+/*
+ As funções e métodos em geral assumem que o valor INT_MAX representa
+ ou a distância infinita ou a inexistência da aresta a depender da função/método
+*/
+
+//Função para calcular a distância entre dois pontos
 float pointDistance(int x_inicial, int y_incial, int x_final, int y_final);
 
+//Função para transformar um (x, y) no range 0 - 10201
 inline int correctCoordinates(int x, int y){
 	return ((x+50)*TAMMATRIX)+(y+50);
 }
 
+//Struct para poder sobrecarregar o operador > para o tipo pair<int, float>
+// considerando o float como item de comparação
+//Para ser usado na fila de prioridades
 struct Comparator
 {
   bool operator()(const pair<int, float> &p1, const pair<int, float> &p2) const
@@ -35,19 +49,25 @@ struct Comparator
   }
 };
 
+//Classe Edge para guardar a distância entre dois vértices
 class Edge{
 	public:
 		float distance;
 
+		//Construtor para iniciar a distância como infinita
 		Edge(){
 			this->distance = INT_MAX;
 		}
 };
 
+//Classe Graph para guardar um grafo
 class Graph{
 	private:
+		//Guarda um grafo em uma matriz de adjacência com tamanho NUMVERTEX x NUMVERTEX
 		vector< vector<Edge> > grafo;
 	public:
+		//Chama o construtor da variável grafo
+		// e cria todas as arestas com as respectivas distâncias
 		Graph() : grafo(NUMVERTEX, vector<Edge>(NUMVERTEX)){
 			for (int x = -50; x <= 50; x++) {
 				for (int y = -50; y <= 50; y++) {
@@ -72,57 +92,15 @@ class Graph{
 			}
 		}
 
-		void addEdge(int x_inicial, int y_inicial, int x_final, int y_final){
-			int org = correctCoordinates(x_inicial, y_inicial);
-			int dest = correctCoordinates(x_final, y_final);
-			grafo[org][dest].distance = grafo[dest][org].distance = pointDistance(x_inicial, y_inicial, x_final, y_final);
-		}
+		//Método para adicionar uma aresta
+		void addEdge(int x_inicial, int y_inicial, int x_final, int y_final);
 
-		void removeVertex(int x, int y){
-			int vertex = correctCoordinates(x, y);
-			for(int i = 0; i < NUMVERTEX; i++){
-				grafo[vertex][i].distance = INT_MAX;
-				grafo[i][vertex].distance = INT_MAX;
-			}
-		}
+		//Método para remover um vértice do grafo
+		void removeVertex(int x, int y);
 
-		float dijkstra(int x_inicial, int y_incial, int x_final, int y_final){
-			float dist[NUMVERTEX];
-			priority_queue< pair<int, float>, vector<pair<int, float> >, Comparator > pq;
-
-			int org = correctCoordinates(x_inicial, y_incial);
-			int dest = correctCoordinates(x_final, y_final);
-
-			for (int i = 0; i < NUMVERTEX; i++) {
-				dist[i] = INT_MAX;
-			}
-
-			dist[org] = 0.0f;
-
-			pq.push(make_pair(org, dist[org]));
-
-			while(!pq.empty()){
-				int u = pq.top().first;
-				float w = pq.top().second;
-
-				if(u == dest) break;
-
-				pq.pop();
-
-				for(int i = 0; i < NUMVERTEX; i++){
-					if(grafo[u][i].distance == INT_MAX)
-						continue;
-
-					float alt = w + grafo[u][i].distance;
-					if(alt < dist[i]){
-						dist[i] = alt;
-						pq.push(make_pair(i, dist[i]));
-					}
-				}
-			}
-
-			return dist[dest];
-		}
+		//Método que utiliza o algoritmo de djikstra para achar
+		// a distância entre dois pontos
+		float dijkstra(int x_inicial, int y_incial, int x_final, int y_final);
 };
 
 #endif
