7e1c0bbf2f15aec0de948657ba720c46d6605399
Yan Uehara <yanuehara@hotmail.com>
2017-01-31 19:44:21 -0300

    Comentaries added
    Signed-off-by: Yan Uehara <yanuehara@hotmail.com>
    Reviewed-By: Flavio Augusto Muller <f.a.m.shinzato@gmail.com>

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@
 *
 * Trabalho 1 de Grafos
 * Prof. Said Sadique
+*
+* main.cpp
+* Arquivo principal
+*
 ***********************************************************/
 
 #include <cstdlib>
@@ -20,20 +24,23 @@ int main(int argc, char* argv[]){
 	int x_obstaculo, y_obstaculo;
 
 	while(true){
-		Graph *grafo = new Graph();
+		Graph *grafo = new Graph(); //Instancia um grafo e cria todas as arestas possíveis
 		cin >> x_inicial >> y_inicial >> x_final >> y_final >> n_obstaculos;
 		if(x_inicial == x_final && x_inicial == 0 && y_final == y_inicial && y_final == 0 && n_obstaculos == 0)
 			break;
 
 		for(int i = n_obstaculos; i != 0; i--){
 			cin >> x_obstaculo >> y_obstaculo;
-			grafo->removeVertex(x_obstaculo, y_obstaculo);
+			grafo->removeVertex(x_obstaculo, y_obstaculo); //Remove os obstáculos
 		}
+
+		//Chama o método disjkstra e retorna o resultado
 		float dist = grafo->dijkstra(x_inicial, y_inicial, x_final, y_final);
 
+		//Seta a precisão para duas casas decimais
 		cout << fixed << setprecision(2);
 		cout << dist << endl;
 
-		delete grafo;
+		delete grafo; //Deleta o grafo
 	}
 }
