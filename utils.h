9825bf9fcdc47f73806479dab8b5685c6faa479d
Yan Uehara <yanuehara@hotmail.com>
2017-01-30 21:17:04 -0300

    Corrected pointDistance call in addEdge
    Signed-off-by: Yan Uehara <yanuehara@hotmail.com>

--- utils.h
+++ utils.h
@@ -65,7 +65,7 @@ class Graph{
 		void addEdge(int x_inicial, int y_inicial, int x_final, int y_final){
 			int org = correctCoordinates(x_inicial, y_inicial);
 			int dest = correctCoordinates(x_final, y_final);
-			grafo[org][dest].distance = grafo[dest][org].distance = pointDistance(x_inicial, x_final, y_inicial, y_final);
+			grafo[org][dest].distance = grafo[dest][org].distance = pointDistance(x_inicial, y_inicial, x_final, y_final);
 		}
 
 		void removeVertex(int x, int y){
