521b21a1a5201b951157dd18f61a891c31500871
Flávio Augusto Muller <f.a.m.shinzato@gmail.com>
2017-01-31 11:02:40 -0300

    Added the comentaries for the utils.cpp file.

--- utils.cpp
+++ utils.cpp
@@ -1,3 +1,11 @@
+/********************************************************
+*
+* Flavio Shinzato
+* Yan Uehara
+*
+* Trabalho 1 de Grafos
+* Prof. Said Sadique
+***********************************************************/
 #include  "utils.h"
 
 float pointDistance(int x_inicial, int y_inicial, int x_final, int y_final){
